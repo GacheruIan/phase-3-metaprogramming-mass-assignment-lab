ff964dfb6f260452ec75f4a160d5cae4804ff3a8
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-10-26 20:56:54 +0300

    Done with assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
-  # your code here
+  def initialize(args)
+    #iterate every instance instead of hard coding making code flexible (room for growth)
+    args.each do |key, value|
+      #setting our attr accessor here
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
